fce9b6a05b308938b952f77e61ca4e1b8f335072
JavaNoobGuy <116377217+javaNoobGuy@users.noreply.github.com>
2023-02-14 05:41:12 -0300

    Add files via upload

---
+++ javaScript/main.js
@@ -0,0 +1,44 @@
+
+const cepValido = (cep) => {
+    if (cep.length = 8) { 
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+const preencherForm = (endereco) =>{
+
+    let bairroInput = document.getElementById('bairro');
+    bairroInput.value = endereco.bairro;
+
+    let cidadeInput = document.getElementById('cidade');
+    cidadeInput.value = endereco.localidade;
+
+
+
+
+
+}
+
+const getApiJson = async(cep) => {
+
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    console.log(url);
+    console.log(cep);
+    if (cepValido(cep)) {
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        console.log(endereco);
+        preencherForm(endereco);
+    }
+    
+}
+
+const addEventoOnCepInput = () =>{
+    let cepInput = document.getElementById('cep');
+    cepInput.addEventListener('focusout',getApiJson(cepInput.value));
+}
+
+addEventoOnCepInput();
